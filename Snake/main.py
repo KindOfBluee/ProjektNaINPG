3ab34cee8380e5a6839e769a0e12c37532f435b5
Janusz123 <38726135+Janusz123@users.noreply.github.com>
2018-05-21 21:01:48 +0200

    Update main.py
    Added Initializing values

--- Snake/main.py
+++ Snake/main.py
@@ -3,3 +3,14 @@
 
 from curses import KEY_RIGHT, KEY_LEFT, KEY_UP, KEY_DOWN
 from random import randint
+
+curses.initscr()
+win = curses.newwin(20, 60, 0, 0)
+win.keypad(1)
+curses.noecho()
+curses.curs_set(0)
+win.border(0)
+win.nodelay(1)
+
+key = KEY_RIGHT                                                    # Initializing values
+score = 0
