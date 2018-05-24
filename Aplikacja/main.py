d814dc57bf656d9d976467f052d3004bbd190b05
KindOfBluee <81320622189a@gmail.com>
2018-05-24 14:43:27 +0200

    Aktualizacja głównego menu

--- Aplikacja/main.py
+++ Aplikacja/main.py
@@ -1,1 +1,10 @@
-print("Hello World")
+import sys
+from PyQt5 import QtWidgets
+
+def window():
+    app = QtWidgets.QApplication(sys.argv)
+    w = QtWidgets.QWidget()
+    w.show()
+    sys.exit(app.exec_())
+
+window()
