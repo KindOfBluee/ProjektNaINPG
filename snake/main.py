b52e076fbd6fab2abcf4fdcb6218d25a0dea070c
Janusz123 <38726135+Janusz123@users.noreply.github.com>
2018-06-06 23:16:45 +0200

    Update main.py

--- snake/main.py
+++ snake/main.py
@@ -41,3 +41,13 @@ while key != 27:                                                   # While Esc k
 
          if key not in [KEY_LEFT, KEY_RIGHT, KEY_UP, KEY_DOWN, 27]:     # If an invalid key is pressed
         key = prevKey
+        
+         # Calculates the new coordinates of the head of the snake. NOTE: len(snake) increases.
+    # This is taken care of later at [1].
+    snake.insert(0, [snake[0][0] + (key == KEY_DOWN and 1) + (key == KEY_UP and -1), snake[0][1] + (key == KEY_LEFT and -1) + (key == KEY_RIGHT and 1)])
+
+    # If snake crosses the boundaries, make it enter from the other side
+    if snake[0][0] == 0: snake[0][0] = 18
+    if snake[0][1] == 0: snake[0][1] = 58
+    if snake[0][0] == 19: snake[0][0] = 1
+    if snake[0][1] == 59: snake[0][1] = 1
