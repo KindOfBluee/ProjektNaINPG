be44994a6e4d44969c9a70a845bd44a50a9c8eb4
KindOfBluee <81320622189a@gmail.com>
2018-06-07 09:03:06 +0200

    zmiana

--- Aplikacja/main.py
+++ Aplikacja/main.py
@@ -1,10 +1,14 @@
 import sys
 from PyQt5 import QtWidgets
 
+print("halo")
+
+
 def window():
     app = QtWidgets.QApplication(sys.argv)
     w = QtWidgets.QWidget()
     w.show()
     sys.exit(app.exec_())
+
 
 window()
