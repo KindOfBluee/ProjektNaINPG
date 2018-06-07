c255cd899474e26fadda88231cf28256521e4557
Tomasz Krawczyk <tkraw97@gmail.com>
2018-06-07 08:49:25 +0200

    stable, working, bug free,cosmetic updates
    Signed-off-by: Tomasz Krawczyk <tkraw97@gmail.com>

--- Pong/pong.py
+++ Pong/pong.py
@@ -1,6 +1,6 @@
 """ CONTROLS:
 Player on the left : Key "z" = UP, Key "s" = DOWN 
-Player on the right : Arrow UP = UP, Arrow Down = DOW"""
+Player on the right : Arrow UP = UP, Arrow Down = DOWN"""
 
 
 
